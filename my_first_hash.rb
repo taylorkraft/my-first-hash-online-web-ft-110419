09831cd5e0c9e3cc93eae2661dd5f4c5ca4664dc
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 18:33:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,10 +18,10 @@ end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+     "whale bone corsets" => 5, 
+     "porcelain vases" => 2, 
+     "oil paintings" => 3
+  }
                  
 shipping_manifest["oil paintings"]
 
@@ -29,11 +29,13 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+     "whale bone corsets" => 5, 
+     "porcelain vases" => 2, 
+     "oil paintings" => 3
+  }
+                 
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
+  
 shipping_manifest
 end
