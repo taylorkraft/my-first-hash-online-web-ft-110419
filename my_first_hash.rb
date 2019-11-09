483e109bde288a494509c54d6f2482e52d239131
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 18:29:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,19 @@
 def my_hash
-  my_hash = {"key" => "value", "key_two" => "value_two"}
+  my_hash = {
+    "key" => "value", 
+    "key_two" => "value_two"
+  }
+  
 end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3} 
-
-
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  } 
+  
 end
 
 def retrieval
@@ -15,8 +22,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+                 
+shipping_manifest["oil paintings"]
 
 end
 
